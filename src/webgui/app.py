decb625bda532b790c0dbb4604307264b12601f2
NxtPerfect <alakaxan@protonmail.com>
2024-06-23 19:20:05 +0200

    Adding app routes

--- src/webgui/app.py
+++ src/webgui/app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from src.stats.stats import readStats
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hacker News Picker</h1>"
+    return render_template("home.html")
+    # return "<h1>Hacker News Picker</h1>"
+
+@app.route('/model')
+def model():
+    data = readStats()
+    return f"{data}"
+
+with app.test_request_context():
+    print(url_for('model'))
